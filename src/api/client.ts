f5496956cc4bdd327832d52a2b9aa03713d33446
annbuch <aniabuchkina3409@gmail.com>
2026-07-06 18:52:21 +0300

    feat: update API integration and improve DebtorsTable component; refactor routing

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
 
-    baseURL:"https://api.bankrot.gov.by/v1",
+    baseURL: "/api/v1",
 
     headers:{
         Authorization:import.meta.env.VITE_API_TOKEN,
--- src/components/DebtorsTable/DebtorsTable.tsx
+++ src/components/DebtorsTable/DebtorsTable.tsx
@@ -1,37 +1,120 @@
 import {
-    Paper,
+
     Table,
-    TableBody,
-    TableCell,
+
     TableHead,
+
+    TableBody,
+
     TableRow,
+
+    TableCell,
+
+    CircularProgress,
+
+    Alert,
+
+    Paper,
+
+    Link,
+
 } from "@mui/material";
 
+import { Link as RouterLink } from "react-router-dom";
+
+import { useAppSelector } from "../../store/hooks";
+import {
+    getRegionName,
+    getProcedureName,
+} from "../../utils/formatters";
+
 export default function DebtorsTable() {
+
+    const {
+
+        items,
+
+        loading,
+
+        error,
+
+    } = useAppSelector(state => state.cases);
+
+    if (loading)
+
+        return (
+
+            <Paper sx={{ p: 5, mt: 3, textAlign: "center" }}>
+
+                <CircularProgress />
+
+            </Paper>
+
+        );
+
+    if (error)
+
+        return (
+
+            <Alert severity="error">
+
+                {error}
+
+            </Alert>
+
+        );
+
+    if (!items.length)
+
+        return (
+
+            <Alert severity="info">
+
+                Ничего не найдено
+
+            </Alert>
+
+        );
+
     return (
-        <Paper
-            elevation={0}
-            sx={{
-                borderRadius: 3,
-                border: "1px solid #E5E7EB",
-                overflow: "hidden",
-            }}
-        >
+
+        <Paper sx={{ mt: 3 }}>
+
             <Table>
 
                 <TableHead>
 
                     <TableRow>
 
-                        <TableCell>Дата возбуждения</TableCell>
+                        <TableCell>
 
-                        <TableCell>Должник</TableCell>
+                            Дата возбуждения
 
-                        <TableCell>Управляющий</TableCell>
+                        </TableCell>
 
-                        <TableCell>Регион</TableCell>
+                        <TableCell>
 
-                        <TableCell>Процедура</TableCell>
+                            Должник
+
+                        </TableCell>
+
+                        <TableCell>
+
+                            Управляющий
+
+                        </TableCell>
+
+                        <TableCell>
+
+                            Регион
+
+                        </TableCell>
+
+                        <TableCell>
+
+                            Процедура
+
+                        </TableCell>
 
                     </TableRow>
 
@@ -39,10 +122,89 @@ export default function DebtorsTable() {
 
                 <TableBody>
 
+                    {
+
+                        items.map(item => (
+
+                            <TableRow key={item.id}>
+
+                                <TableCell>
+
+                                    {item.startDate}
+
+                                </TableCell>
+
+                                <TableCell>
+
+                                    <Link
+
+                                        component={RouterLink}
+
+                                        to={`/debtors/:id`}
+
+                                    >
+
+                                        {
+
+                                            item.debtorModel.organization.shortName
+
+                                        }
+
+                                    </Link>
+
+                                </TableCell>
+
+                                <TableCell>
+
+                                    {item.manager.name}
+
+                                </TableCell>
+
+                                <TableCell>
+
+                                    {
+
+                                        getRegionName(
+
+                                            item.debtorModel.organization.legalAddress.region
+
+)
+                                    }
+
+                                </TableCell>
+
+                                <TableCell>
+
+                                    {
+
+                                        item.status === 0
+
+                                            ? "Дело закрыто"
+
+                                            : getProcedureName(
+
+                                                item.procedureType,
+
+                                                item.status
+
+                                            )
+
+                                    }
+
+                                </TableCell>
+
+                            </TableRow>
+
+                        ))
+
+                    }
+
                 </TableBody>
 
             </Table>
 
         </Paper>
+
     );
+
 }
